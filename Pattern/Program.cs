ea78e044c849c4b2a5b1caf198b369fde6828700
fintalex <fintalex@mail.ru>
2014-12-09 12:30:06 +0300

    Singleton and Factory method

---
+++ Pattern/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// ======================== Singleton==========================
+			//Singleton single = Singleton.GetInstanse();
+			//single.flag = true;
+			//Console.WriteLine("single.flag = " + single.flag);
+
+			//Singleton single2 = Singleton.GetInstanse();
+			//single2.flag = false;
+			//Console.WriteLine(" Выполнилась операция 'single2.flag = false'");
+
+			//Console.WriteLine(" А изменилось поле single.flag = " + single.flag);
+
+			//Console.ReadLine();
+			// ========================// ========================// ========================
+
+			// ======================== Factory Method==========================
+			var factory = new FordFiestaFactory() as ICreateCars;
+
+			var myCar = factory.CreateACar("red");
+
+			Console.WriteLine("Make: " + myCar.Make + " " + myCar.Model + " " + myCar.Colour);
+
+			Console.ReadLine();
+			// ========================// ========================// ========================
+		}
+	}
+
+	// ======================== Singleton==========================
+	#region singleton
+	class Singleton
+	{
+		private static Singleton _intstance = new Singleton();
+
+		private Singleton() { }
+
+		public static Singleton GetInstanse()
+		{
+			return _intstance;
+		}
+		public bool flag; // может быть любой член
+	} 
+	#endregion
+	// ========================// ========================// ========================
+
+	// ======================== Factory Method==========================
+	// Паттерн создания объектов. Предоставляет инструменты для создания экземпляров некоторого класса
+	// При создании объекта наследники могут определить какого же класса создавать экземплят. 
+	// ФАБРИКА предоставляет создание объектов наследникам базового класса. 
+	// Это позволяет использовать в программе не конкретные классы, а манипулировать абст-ми объектами
+
+	#region FactoryMethod reader 
+	// данный класс могут наследовать другие
+	public interface IReader
+	{
+		void readData(Object input);
+	}
+
+	// создадим три класса
+	class ReaderDataBase : IReader
+	{
+		public void readData(object input)
+		{
+			throw new NotImplementedException();
+		}
+	}
+	class ReaderTextFile : IReader
+	{
+		public void readData(object input)
+		{
+			throw new NotImplementedException();
+		}
+	}
+	class ReaderBinaryFile : IReader
+	{
+		public void readData(object input)
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	// для создания необходимого объекта (это и есть сам фабричный метод)
+	class FactoryMethod
+	{
+		public IReader getCurrentReader(Object input)
+		{
+			if (input is ReaderDataBase)
+			{
+				return new ReaderDataBase();
+			}
+			else if (input is ReaderBinaryFile)
+			{
+				return new ReaderBinaryFile();
+			}
+			else
+			{
+				return new ReaderTextFile();
+			}
+		}
+	} 
+	#endregion
+
+	#region FactoryMethod Car
+	// немного другая реализация ФАБРИЧНОГО МЕТОДА
+	interface ICreateCars
+	{
+		Car CreateACar(string colour);
+	}
+	class FordFiestaFactory : ICreateCars
+	{
+		public Car CreateACar(string colour)
+		{
+			return new FordFiesta() { Colour = colour };
+		}
+	}
+
+	public abstract class Car
+	{
+		public string Make { get; set; }
+		public string Model { get; set; }
+		public string EngineSize { get; set; }
+		public string Colour { get; set; }
+	}
+
+	public class FordFiesta : Car
+	{
+		public FordFiesta()
+		{
+			Make = "Ford";
+			Model = "fiesta";
+			EngineSize = "1.1";
+		}
+	} 
+	#endregion
+}
---
+++ Pattern/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Pattern")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Pattern")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("558a5be7-cafa-419c-9cd2-091f3ae90139")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер построения
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
