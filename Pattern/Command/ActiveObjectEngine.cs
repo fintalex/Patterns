c4880dbed6169941a0923bfa4e0dd49998a945bb
fintalex <fintalex@mail.ru>
2015-02-24 10:08:20 +0300

    ActiveObjectEngine

---
+++ Pattern/Command/ActiveObjectEngine.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern.Command
+{
+	public class ActiveObjectEngine
+	{
+		ArrayList itsCommands = new ArrayList();
+		public void AddCommand(Command c)
+		{
+			itsCommands.Add(c);
+
+		}
+
+		public void Run()
+		{
+			while (itsCommands.Count > 0)
+			{
+				Command c = (Command)itsCommands[0];
+				itsCommands.RemoveAt(0);
+				c.Execute();
+			}
+		}
+	}
+	public interface Command
+	{
+		void Execute();
+	}
+}
---
+++ Pattern/Command/Command.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command.Pattern
+{
+	// Он инкапсулирует единственную функцию, сво-
+	// бодную от каких бы то ни было переменных.
+	public interface Command
+	{
+		void Execute();
+		void Undo();
+	}
+	// И это уже является паттерном Команда
+}
