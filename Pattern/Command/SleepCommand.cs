21ce5cb2e14db612c9e21676ea62f135c202d269
fintalex <fintalex@mail.ru>
2015-02-24 10:38:33 +0300

    cteating test

---
+++ Pattern/Command/SleepCommand.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pattern.Command
+{
+	public class SleepCommand : Command
+	{
+		private Command wakeupCommand = null;
+		private ActiveObjectEngine engine = null;
+		private long sleepTime = 0;
+		private DateTime startTime;
+		private bool started = false;
+
+		public SleepCommand(long milliseconds, ActiveObjectEngine e, Command wakeupCommand)
+		{
+			sleepTime = milliseconds;
+			engine = e;
+			this.wakeupCommand = wakeupCommand;
+		}
+
+		public void Execute()
+		{
+			DateTime currentTime = DateTime.Now;
+			if (!started)
+			{
+				started = true;
+				startTime = currentTime;
+				engine.AddCommand(this);
+			}
+			else
+			{
+				TimeSpan elapsedTime = currentTime - startTime;
+				if (elapsedTime.TotalMilliseconds < sleepTime)
+				{
+					engine.AddCommand(this);
+				}
+				else
+				{
+					engine.AddCommand(wakeupCommand);
+				}
+			}
+		}
+	}
+}
