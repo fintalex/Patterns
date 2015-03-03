9befd0a05eafaaae4c0c238a2cdd2e1a5adfb5c9
fintalex <fintalex@mail.ru>
2015-03-03 10:05:01 +0300

    singleton

---
+++ Singleton/Singleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+	// это простой singleton
+	// характерность :
+	// 1. конструктор приватный который может вызываться только внутри
+	// 2. статическое свойство объекта, значение которому присваивается только один раз
+
+	// Вместо singletion instance может быть любое свойство которое нам нужно
+	public class Singleton
+	{
+		private static Singleton _instance = null;
+		private Singleton()	{}
+
+		public static Singleton Instance 
+		{
+			get {
+				if (_instance == null)
+					new Singleton();
+				return _instance;
+			}
+		}
+	}
+}
---
+++ Singleton/SingletonTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System.Reflection;
+
+namespace SingletonPattern
+{
+	[TestFixture]
+	public class SingletonTest
+	{
+		[Test]
+		public void TestSingletonTheSameDuringLive()
+		{
+			Singleton s1 = Singleton.Instance;
+			Singleton s2 = Singleton.Instance;
+			Assert.AreEqual(s1, s2);
+		}
+
+		[Test]
+		public void TestSingletionDoesNotHavePublicConstructor()
+		{
+			Type typeSingleton = typeof(Singleton);
+			ConstructorInfo[] ctrs = typeSingleton.GetConstructors();
+			bool ourSingletonHavePublishConstructor = false;
+			foreach (ConstructorInfo c in ctrs)
+			{
+				if (c.IsPublic)
+				{
+					ourSingletonHavePublishConstructor = true;
+					break;
+				}
+			}
+			Assert.IsFalse(ourSingletonHavePublishConstructor);
+		}
+	}
+}
