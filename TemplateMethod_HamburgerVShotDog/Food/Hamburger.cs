9e31d6de8966e3033c42a2c1e69eb680b6bd53ad
fintalex <fintalex@mail.ru>
2015-03-05 17:32:30 +0300

    TemplateMethod
    Гамбургер и хот дог

---
+++ TemplateMethod_HamburgerVShotDog/Food/Hamburger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethod_HamburgerVShotDog.Food
+{
+	class Hamburger
+	{
+		public void Prepare()
+		{
+			RoastBred();
+			FryMeat();
+			PutVegetables();
+			AddKetchup();
+		}
+
+		public void AddKetchup()
+		{
+			Console.WriteLine("AddKetchup");
+		}
+		public void PutVegetables()
+		{
+			Console.WriteLine("vegetables");
+		}
+		public void FryMeat()
+		{
+			Console.WriteLine("FryMeat");
+		}
+		public void RoastBred()
+		{
+			Console.WriteLine("bread");
+		}
+	}
+}
---
+++ TemplateMethod_HamburgerVShotDog/Food/HotDog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethod_HamburgerVShotDog.Food
+{
+	class HotDog
+	{
+		public void Prepare()
+		{
+			RoastBred();
+			BoilSausage();
+			PutVegetables();
+			AddMustard();
+		}
+
+		public void AddMustard()
+		{
+			Console.WriteLine("Musstard");
+		}
+		public void PutVegetables()
+		{
+			Console.WriteLine("vegetables");
+		}
+		public void BoilSausage()
+		{
+			Console.WriteLine("sausage");
+		}
+		public void RoastBred()
+		{
+			Console.WriteLine("bread");
+		}
+	}
+}
